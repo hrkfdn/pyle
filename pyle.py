83db0580ace5507e57bb6e5a3ffcef404f70d95a
Henrik Friedrichsen <henrik@affekt.org>
2014-05-23 00:23:21 +0200

    prototype for images

---
+++ pyle.py
@@ -0,0 +1,30 @@
+# Configuration
+STATIC_PATH = "/home/henrik/webdump"
+
+from mimetypes import MimeTypes
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "nope."
+
+@app.route("/<file>/data")
+def readfile(file):
+    return send_from_directory(STATIC_PATH, file)
+
+@app.route("/<file>/")
+def showfile(file):
+    mime = MimeTypes()
+    mime_type = mime.guess_type(file)
+    if mime_type[0] is not None and mime_type[0].startswith("image"):
+        return render_template("image.html",
+                title="pyle :: preview",
+                filename=file,
+                filepath="/" + file + "/data")
+    else:
+        return readfile(file)
+
+if __name__ == "__main__":
+    app.run(debug=True)
